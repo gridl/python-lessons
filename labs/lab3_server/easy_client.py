5f5417fcb673eff15c575ea020e82a5642dc2565
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2015-10-20 10:31:13 +0300

    labs

--- labs/lab3_server/easy_client.py
+++ labs/lab3_server/easy_client.py
@@ -6,8 +6,8 @@ PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 data = 'Hello world'
-s.send(data)
-print 'Send[1]: ', data
+s.send(data.encode('utf-8'))
+print('Send[1]: ', data)
 data = s.recv(1024)
 s.close()
-print 'Received[4]: ', data
+print('Received[4]: ', data)
--- labs/lab3_server/easy_server.py
+++ labs/lab3_server/easy_server.py
@@ -8,13 +8,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
-print 'Connected client'
+print('Connected client')
 while 1:
     data = conn.recv(1024)
     if not data:
         break
     else:        
-        print 'Received[2]: ', data    
+        print('Received[2]: ', data)    
     conn.send(data)
-    print 'Send[3]: ', data
+    print('Send[3]: ', data)
 conn.close()
